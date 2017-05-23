b52f3fe5ce87c2fb7ec1bb2de0722765759764ae
Kiwi <kerwin.kaiwei.huang@gmail.com>
2017-05-23 23:21:18 +0800

    Calculation of Implied Volatility.

--- ImpliedVol.py
+++ ImpliedVol.py
@@ -1,14 +1,18 @@
 '''
 MIT License
+
 Copyright (c) 2017 Kiwi
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,7 +26,8 @@ SOFTWARE.
 
 '''
 This function computes the implied volatility of options based on the assumption of Black-Scholes-Merton model.
-Under Black-Scholes-Merton model, there is a closed-form solution when implementing Newton-Raphson Method to calculate implied volatitlity.
+Under Black-Scholes-Merton model, there is a closed-form solution
+when implementing Newton-Raphson Method to calculate implied volatitlity.
 About dividend: the default setting is no dividend included.
     User can add dividend calculation with known dividend payment in the holding period.
     Type1: D = [rate,'Con'], which is the continuous dividend payment over certain period with a constant payment rate.
